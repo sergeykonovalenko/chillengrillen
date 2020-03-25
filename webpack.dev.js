59eacea2512b0a2349a3baf58b6b83e925aecab7
Sergey Konovalenko <sergeykonovalenko5550199@gmail.com>
2020-03-25 07:11:59 +0200

    Старт

---
+++ webpack.dev.js
@@ -0,0 +1,125 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+
+let devServer; // set below
+
+module.exports = (env, argv) => {
+
+    return merge(common(argv.mode), {
+        devtool: 'inline-source-map',
+        // devtool: 'inline-cheap-module-source-map',
+        devServer: {
+            before(app, server) {
+                devServer = server;
+            },
+            contentBase: './dist',
+            overlay: {
+                warnings: true,
+                errors: true
+            },
+            host: '192.168.0.100',
+            port: 8081,
+            hot: true,
+            writeToDisk: true,
+        },
+        watchOptions: {
+            aggregateTimeout: 100,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader,
+                            options: {
+                                hmr: true,
+                                reloadAll: true,
+                            },
+                        },
+                        // 'style-loader',
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true,
+                                url: false,
+                            },
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sassOptions: {
+                                    indentWidth: 4,
+                                    outputStyle: 'compressed',
+                                    // outputStyle: 'expanded',
+                                },
+                                sourceMap: true,
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.css$/i,
+                    use: [
+                        // 'style-loader',
+                        {
+                            loader: MiniCssExtractPlugin.loader,
+                            options: {
+                                hmr: true,
+                                reloadAll: true,
+                            },
+                        },
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true,
+                                url: false,
+                            },
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                sourceMap: true,
+                            }
+                        },
+                    ],
+                }
+            ],
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin(),
+            new MiniCssExtractPlugin({
+                // filename: 'css/[name].css',
+                filename: 'style.css',
+            }),
+            reloadHtml,
+            // new webpack.SourceMapDevToolPlugin({
+            //     filename: "[file].map"
+            // }),
+        ],
+    });
+};
+
+// Reload HTML
+function reloadHtml() {
+    this.plugin('compilation',
+        thing => thing.plugin('html-webpack-plugin-after-emit', trigger));
+    const cache = {};
+    function trigger(data, callback) {
+        const orig = cache[data.outputName];
+        const html = data.html.source();
+        // plugin seems to emit on any unrelated change?
+        if (orig && orig !== html)
+            devServer.sockWrite(devServer.sockets, 'content-changed');
+        cache[data.outputName] = html;
+        callback();
+    }
+}
