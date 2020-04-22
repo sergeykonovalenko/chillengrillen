d15cc1dda54f57572fe821438fc43242ed88000e
Sergey Konovalenko <sergeykonovalenko5550199@gmail.com>
2020-04-22 18:37:16 +0300

    Сделал страницу аккаунта -Консоль- и -Заказы-

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,6 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
 
@@ -23,7 +24,7 @@ module.exports = (env, argv) => {
         devtool: 'inline-source-map',
         // devtool: 'inline-cheap-module-source-map',
         devServer: {
-            contentBase: './dist',
+            contentBase: path.join(__dirname, 'dist'),
             overlay: {
                 warnings: true,
                 errors: true
